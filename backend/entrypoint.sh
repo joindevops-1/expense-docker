35571c4062143b9d05b5e68a5115bb531e143611
joindevops <info@joindevops.com>
2024-07-12 09:28:43 +0530

    functions

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
-# Script to adjust ndots in /etc/resolv.conf
-
-# Check if we can write to /etc/resolv.conf
-if [ -w /etc/resolv.conf ]; then
-  if grep -q "options ndots:" /etc/resolv.conf; then
-      sed -i 's/options ndots:[0-9]*/options ndots:2/' /etc/resolv.conf
-  else
-      echo "options ndots:2" >> /etc/resolv.conf
-  fi
-else
-  echo "Cannot modify /etc/resolv.conf, read-only file system."
-fi
-
 # Start the Node.js application
 exec node index.js

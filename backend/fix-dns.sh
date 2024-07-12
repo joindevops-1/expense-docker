d1a568d0eef34aa32f31162dad69359b4ef643af
joindevops <info@joindevops.com>
2024-07-12 09:25:23 +0530

    functions

--- backend/fix-dns.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Script to adjust ndots in /etc/resolv.conf
-if grep -q "options ndots:" /etc/resolv.conf; then
-    sed -i 's/options ndots:[0-9]*/options ndots:2/' /etc/resolv.conf
-else
-    echo "options ndots:2" >> /etc/resolv.conf
-fi
